f1ef9609437a7cd0b5853919b0ee9f3a81e2a384
Sadman26 <fareless.ranger@gmail.com>
2023-03-28 16:59:50 +0600

    successfully speech to text

--- app/src/main/java/com/example/ajaira/homepage.java
+++ app/src/main/java/com/example/ajaira/homepage.java
@@ -1,16 +1,25 @@
 package com.example.ajaira;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class homepage extends AppCompatActivity {
     FirebaseAuth auth=FirebaseAuth.getInstance();
+    FloatingActionButton micbtn;
     TextView txt;
 
     @SuppressLint({"WrongViewCast", "MissingInflatedId"})
@@ -18,7 +27,40 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        txt=findViewById(R.id.txt);
-        txt.setText(auth.getCurrentUser().getEmail());
+        micbtn=findViewById(R.id.micbtn);
+        txt=findViewById(R.id.txtt);
+        micbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speak();
+            }
+        });
+
+    }
+
+    private void speak() {
+        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"en-US");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speak Now");
+        try{
+            startActivityForResult(intent,1);
+        }
+        catch (Exception e){
+            Toast.makeText(this, "Error "+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 1:
+                if(resultCode==RESULT_OK && data!=null){
+                    ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txt.setText(result.get(0));
+                }
+                break;
+        }
     }
 }
